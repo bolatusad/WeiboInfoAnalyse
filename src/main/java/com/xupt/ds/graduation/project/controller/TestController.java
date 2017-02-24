d53fcef3d8159970dbd1da4ae2016b9692f66c57
zxh <176078936@qq.com>
2017-02-24 16:20:07 +0800

    提交基本的maven文件结构
    Change-Id: I78f51ae1a522aa3a620799a6822a486284fb28a3
    Signed-off-by: zxh

---
+++ src/main/java/com/xupt/ds/graduation/project/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.xupt.ds.graduation.project.controller;
+
+/**
+ * @Title:TestController
+ * @Description:用于环境搭建的Controller
+ * @author: zxh
+ * @Date：2017年2月24日下午4:10:09
+ *
+ */
+public class TestController {
+
+}
