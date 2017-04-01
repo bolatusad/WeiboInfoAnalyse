9b6c7c4a530831355df5d26b546b86b70e12f97d
bolatusad <563375002@qq.com>
2017-04-01 09:07:32 +0800

    测试git是否可用

---
+++ src/main/java/com/xupt/zxh/graduation/project/dao/account/UserDao.java
@@ -0,0 +1,11 @@
+package com.xupt.zxh.graduation.project.dao.account;
+
+import com.xupt.zxh.graduation.project.bean.account.User;
+
+/**
+ * Created by admin on 2017/4/1.
+ */
+public interface UserDao {
+
+    User getUserByEmail();
+}
---
+++ src/main/java/com/xupt/zxh/graduation/project/service/account/IUserService.java
@@ -0,0 +1,8 @@
+package com.xupt.zxh.graduation.project.service.account;
+
+/**
+ * Created by admin on 2017/4/1.
+ */
+public interface IUserService {
+
+}
